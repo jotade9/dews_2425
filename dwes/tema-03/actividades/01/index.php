4ea947ebb945ff34b1800c46ab9b894ffd95513f
jotade9 <160598704+jotade9@users.noreply.github.com>
2024-10-14 09:13:55 +0200

    Add files via upload

---
+++ dwes/tema-03/actividades/01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Actividad 34 - Tema 3 PHP
+ * 
+ * controlador principal
+ * 
+ * carga model.index.php 
+ * carga view.index.php 
+ */
+//mostrar todo el array
+include "models/model.index.php";
+
+include "views/view.index.php";
---
+++ dwes/tema-03/actividades/01/views/view.index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Actividad 3.1 - Tabla de Libros</title>
+    <!-- css bootstrap 533 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- bootstrap icons 1.11.3 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+</head>
+
+<body>
+    <!-- capa principal -->
+    <div class="container">
+
+        <!-- cabecera documento -->
+        <header class="pb-3 mb-4 border-bottom">
+            <i class="bi bi-book"></i>
+            <span class="fs-6">Tabla Libros</span>
+        </header>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Genero</th>
+                    <th scope="col">Precio</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($libros as $registro): ?>
+                    <tr class="">
+                        <th><?= $registro['id'] ?></th>
+                        <td><?= $registro['titulo'] ?></td>
+                        <td><?= $registro['autor'] ?></td>
+                        <td><?= $registro['genero'] ?></td>
+                        <td><?= $registro['precio'] ?>€</td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+
+        <!-- pie de página -->
+        <footer class="footer mt-auto py-3 fixed-bottom bg-light">
+            <div class="container">
+                <span class="text-muted">© 2024
+                    Juan Diego Pérez López - DWES - 2º DAW - Curso 24/25
+                </span>
+            </div>
+        </footer>
+    </div>
+    <!-- javascript bootstrap 533 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
