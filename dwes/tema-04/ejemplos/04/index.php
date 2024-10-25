137217d08b0001c9fef8833c8b78ed15c79604ab
jotade9 <160598704+jotade9@users.noreply.github.com>
2024-10-25 14:04:23 +0200

    Add files via upload

--- dwes/tema-04/ejemplos/04/index.php
+++ dwes/tema-04/ejemplos/04/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 /**
  * Ejemplo
@@ -25,4 +26,33 @@ echo "Matricula: ". $vehiculo->getMatricula();
 echo "<br>";
 echo "Velocidad: ". $vehiculo->getVelocidad();
 
+=======
+<?php
+/**
+ * Ejemplo
+ * Descripción: crear objetos a patir de la clase Class_vehiculo
+ */
+include 'class/class.vehiculo.php';
+# Creo un objeto de la clase vehiculo
+ $vehiculo = new Class_vehiculo();
+
+# Establecer la matricula del vehiculo 6712HRM
+// $vehiculo->setMatricula('6712HRM');
+$vehiculo->matricula = '6712HRM0';
+
+# Establecer velocidad a 10km/h
+// $vehiculo->setVelocidad(10);
+$vehiculo->velocidad = 10;
+// de esta forma no cumple el criterio de encapsulación
+// Para el criterio de encapsulación debemos
+// acceder mediante metodos
+// encapsulacion- atributo private
+// no encapsulación - atributo public
+
+# Mostrar detalles del vehiculo
+echo "Matricula: ". $vehiculo->getMatricula();
+echo "<br>";
+echo "Velocidad: ". $vehiculo->getVelocidad();
+
+>>>>>>> 7c139080c002fe1675da1016599ab6d972ca90e2
  //var_dump($vehiculo);
