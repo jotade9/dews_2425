224898b61a70ac8a7e5c5afc486d2a8dfdedeb04
jotade9 <160598704+jotade9@users.noreply.github.com>
2024-10-30 13:12:12 +0100

    Add files via upload

---
+++ dwes/tema-04/ejemplos/07/index.php
@@ -0,0 +1,32 @@
+<?php
+
+    /*
+        concepto de herencias
+    */
+
+    include "class/class.producto.php";
+
+    $producto = new Class_producto(
+        1,
+        'La tormenta',
+        20.45,
+        'Juan',
+        'Riquelme'
+    );
+
+    $libro = new Class_libro(
+        2,
+        'Ubrique villa olímpica',
+        20.99,
+        'Pedro',
+        'Ortega',
+        456,
+        'Alfaguara' 
+    );
+
+    // $producto -> titulo = 'La tormenta perfecta'; Si queremos modificar una propiedad privada no nos vale esto
+    $producto -> setTitulo('La tormenta perfecta'); // Para modificar una propiedad privada debemos usar los setters
+
+    $libro->muestra_libro();
+
+
---
+++ dwes/tema-04/ejemplos/08/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*
+        concepto de herencias
+    */
+
+    include "class/class.producto.php";
+
+    Class_producto::metodo_estatico();
+    echo Class_producto::$portada;
+
+
