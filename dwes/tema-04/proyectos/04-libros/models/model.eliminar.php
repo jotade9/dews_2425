880701ca79aea3b9a52b24be21ccbac733d67c82
jotade9 <160598704+jotade9@users.noreply.github.com>
2024-11-14 23:39:56 +0100

    14/11

---
+++ dwes/tema-04/proyectos/04-libros/models/model.eliminar.php
@@ -0,0 +1,27 @@
+<?php
+/*
+        modelo:  model.editar.php
+        descripcion: carga los datos del artículo que deseo actualizar
+
+        Método GET
+            - indice
+    */
+
+# Cargamos el id del artículo
+$indice = $_GET['indice'];
+
+# Creo un objeto de la clase tabla de artículo
+$obj_tabla_libros = new Class_tabla_libros();
+
+# Cargo los datos de artículos
+$obj_tabla_libros->getDatos();
+
+# Cargo el array  de materias - lista desplegable dinámica
+$materias = $obj_tabla_libros->getMaterias();
+
+
+#Obtener el objeto de la clase artículo   correspondiente a ese índice
+$obj_tabla_libros->delete($indice);
+
+# Obtengo la tabla de libros actualizada para la vista
+$array_libros = $obj_tabla_libros->tabla;
