84781b5ffe1b40a79f2cd7fdfb8f54c749b28657
jotade9 <160598704+jotade9@users.noreply.github.com>
2024-12-09 20:35:10 +0100

    mostrar

--- dwes/tema-05/proyectos/03-gesbank-pdo/07/models/model.mostrar.php
+++ dwes/tema-05/proyectos/03-gesbank-pdo/07/models/model.mostrar.php
@@ -2,31 +2,22 @@
 
     /*
         modelo: model.mostrar.php
-        descripción: carga los datos del libro que deseo actualizar
+        descripción: carga los datos del cliente que deseo actualizar
 
         Método GET:
 
-            - indice de la tabla en la que se encuentra el libro
+            - id del cliente
     */
 
-    # Cargamos el indice del libro
-    $indice = $_GET['indice'];
+    # Cargamos el id del cliente que vamos a editar
+    $id = $_GET['id'];
 
-    # Creo un objeto de la clase tabla de libros
-    $obj_tabla_libros = new Class_tabla_libros();
+    # Creo un objeto de la clase tabla clientes
+    $conexion = new Class_tabla_clientes();
 
-    #  Cargo los datos de los libros
-    $obj_tabla_libros->getDatos();
-    
-    # Cargo el array de materias - lista desplegable dinámica
-    $materias = $obj_tabla_libros->getMaterias();
 
-    # Cargo el array de etiquetas - lista checbox dinámica
-    $etiquetas = $obj_tabla_libros->getEtiquetas();
+    # Obtener los detalles del cliente 
+    // objeto de la clase cliente
+    $cliente = $conexion->read($id);
 
-    # Obtener el objeto de la clase artículo correspondiente a ese índice
-    $libro = $obj_tabla_libros->read($indice);
-
-    # Forma alternativa por la propiedad de no encapsulamiento
-    // $libro = $obj_tabla_libros->tabla[$indice];
    
--- dwes/tema-05/proyectos/03-gesbank-pdo/07/views/view.mostrar.php
+++ dwes/tema-05/proyectos/03-gesbank-pdo/07/views/view.mostrar.php
@@ -3,7 +3,7 @@
 
 <head>
     <?php include 'views/layouts/layout.head.html'; ?>
-    <title>Mostrar Libro - CRUD Libros </title>
+    <title>Mostrar Cliente - CRUD Clientes </title>
 </head>
 
 <body>
@@ -13,58 +13,45 @@
         <!-- Encabezado proyecto -->
         <?php include 'views/partials/partial.header.php'; ?>
 
-        <legend>Formulario Mostrar Libro</legend>
+        <legend>Formulario Mostrar Cliente</legend>
 
         <!-- Formulario Nuevo libro -->
 
         <form>
 
-            <!-- id -->
+            <!-- Nombre -->
             <div class="mb-3">
-                <label for="id" class="form-label">Id</label>
-                <input type="text" class="form-control" name="id" value="<?= $libro->id?>" disabled>
+                <label for="nombre" class="form-label">Nombre</label>
+                <input type="text" class="form-control" name="nombre" value="<?= $cliente->nombre?>" disabled>
+            </div>
+            <!-- Apellidos -->
+            <div class="mb-3">
+                <label for="apellidos" class="form-label">Apellidos</label>
+                <input type="text" class="form-control" name="apellidos" value="<?= $cliente->apellidos?>" disabled>
             </div>
 
-            <!-- titulo -->
+            <!-- Ciudad -->
             <div class="mb-3">
-                <label for="titulo" class="form-label">Titulo</label>
-                <input type="text" class="form-control" name="titulo" value="<?= $libro->titulo?>" disabled>
+                <label for="ciudad" class="form-label">Ciudad</label>
+                <input type="text" class="form-control" name="ciudad" value="<?= $cliente->ciudad?>" disabled>
             </div>
 
-            <!-- autor -->
+            <!-- Telefono -->
             <div class="mb-3">
-                <label for="autor" class="form-label">Autor</label>
-                <input type="text" class="form-control" name="autor" value="<?= $libro->autor?>" disabled>
+                <label for="telefono" class="form-label">Teléfono</label>
+                <input type="tel" class="form-control" name="telefono" value="<?= $cliente->telefono?>" disabled>
+            </div>
+            
+            <!-- Dni -->
+            <div class="mb-3">
+                <label for="dni" class="form-label">Dni</label>
+                <input type="text" class="form-control" name="dni" value="<?= $cliente->dni?>" disabled>
             </div>
 
-            <!-- editorial -->
+            <!-- Email -->
             <div class="mb-3">
-                <label for="editorial" class="form-label">Editorial</label>
-                <input type="text" class="form-control" name="editorial" value="<?= $libro->editorial?>" disabled>
-            </div>
-
-            <!-- fecha_edicion -->
-            <div class="mb-3">
-                <label for="fecha_edicion" class="form-label">Fecha Edición</label>
-                <input type="date" class="form-control" name="fecha_edicion" value="<?= $libro->fecha_edicion?>" disabled>
-            </div>
-
-            <!-- Precio -->
-            <div class="mb-3">
-                <label for="precio" class="form-label">Precio (€)</label>
-                <input type="number" class="form-control" name="precio" step="0.01" value="<?= $libro->precio?>" disabled>
-            </div>
-
-            <!-- Campo Mostar -->
-            <div class="mb-3">
-                <label for="materia" class="form-label">Materia</label>
-                <input type="text" class="form-control" name="materia" value="<?= $materias[$libro->materia]?>" disabled>
-            </div>
-
-            <!-- Campo Etiquetas -->
-            <div class="mb-3">
-                <label for="etiquetas" class="form-label">Etiquetas</label>
-                <input type="text" class="form-control" name="etiquetas" value="<?= implode(', ', $obj_tabla_libros->mostrar_nombre_etiquetas($libro->etiquetas)) ?>" disabled>
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" value="<?= $cliente->email?>" disabled>
             </div>
 
             <!-- botones de acción -->
