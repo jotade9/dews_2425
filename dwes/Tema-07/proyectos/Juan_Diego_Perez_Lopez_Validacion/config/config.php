0d8150a8bfb65f425aa01761665de699a03d7e1b
jotade9 <160598704+jotade9@users.noreply.github.com>
2025-01-22 17:17:47 +0100

    Cambios locales en config.php y añadido archivo .rar

--- dwes/Tema-07/proyectos/Juan_Diego_Perez_Lopez_Validacion/config/config.php
+++ dwes/Tema-07/proyectos/Juan_Diego_Perez_Lopez_Validacion/config/config.php
@@ -3,13 +3,13 @@
 
 # Ruta absoluta
 
-define('URL', 'http://localhost:8080/dews_2425/dwes/Tema-07/proyectos/01-validacion/');
+define('URL', 'http://localhost:8080/dews_2425/dwes/Tema-07/proyectos/Juan_Diego_Perez_Lopez_Validacion/');
 
 # Constante de la Base de Datos
 define('HOST', 'localhost');
 define('DB', 'fp');
 define('USER', 'root');
-define('PASSWORD', '');
+define('PASSWORD', '1234');
 define('CHARSET', 'utf8mb4');
 
 
